ce914a988e3cddf220a9ddda9bb4f6dd93da14c8
VR Deepak <deepakgates@gmail.com>
2017-08-29 01:50:16 +0530

    google verification

--- application/views/login.php
+++ application/views/login.php
@@ -1,2 +1,9 @@
-<meta name="google-site-verification" content="y_gQW5vEI8b2jNn60Fs3EM9Wu2BTy5bacFA_jmkqFBw" />
+<html>
+<head>
+    <meta name="google-site-verification" content="y_gQW5vEI8b2jNn60Fs3EM9Wu2BTy5bacFA_jmkqFBw" />
+</head>
+<body>
 <a href="/welcome/authenticate">Log In</a>
+</body>
+</html>
+
