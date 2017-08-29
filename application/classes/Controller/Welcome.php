d9035d4705bb1bb0817fb189c8a93a734e11ea49
VR Deepak <deepakgates@gmail.com>
2017-08-29 20:56:15 +0530

    logs 2

--- application/classes/Controller/Welcome.php
+++ application/classes/Controller/Welcome.php
@@ -124,6 +124,7 @@ class Controller_Welcome extends Controller {
     {
         ini_set("log_errors", 1);
         error_log('callback response - '.file_get_contents('php://input'));
+        error_log('callback response2 - '.json_encode($this->request->post()));
      }
 
 
